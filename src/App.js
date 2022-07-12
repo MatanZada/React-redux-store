1a0bfe4d9ebc2ebfc27298c24e6d1205d0594e86
matan <you@example.com>
2022-07-12 22:52:06 +0300

    extended Ridex course, package repair

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./components/home/HomePage";
+import AboutPage from "./components/about/AboutPage";
+import Header from "./components/common/Header";
+import PageNotFound from "./components/PageNotFound";
+import CoursesPage from "./components/courses/CoursesPage";
+import ManageCoursePage from "./components/courses/ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="courses" element={<CoursesPage />} />
+          <Route path="course/:slug" element={<ManageCoursePage />} />
+          <Route path="course" element={<ManageCoursePage />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+        <ToastContainer autoClose={3000} hideProgressBar />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import reportWebVitals from "./reportWebVitals";
+import configureStores from "./redux/configureStores";
+import { BrowserRouter } from "react-router-dom";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { Provider } from "react-redux";
+const store = configureStores();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
